d73769d3c94463d802ba718a763ed51e868b9132
Aziz Light <aziz@azizlight.me>
2012-02-26 22:15:57 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ambition::Application.config.secret_token = '53755bd1b0d4e0c9e88a355631d8c9803c9fc837cb454319a55b11674ea6561326f8c7592c0433f5cad41281e67f0d1e26ab60fb66886152a6d2df695bc76249'
