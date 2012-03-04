e1138ba8099e4683baf3c486475a12b61b6848f6
Aziz Light <aziz@azizlight.me>
2012-03-04 17:20:00 +0000

    removed a call to p

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,8 +10,6 @@ class UserMailer < ActionMailer::Base
     @user = user
     @url = "http://localhost.3000/password_resets/#{user.reset_password_token}/edit"
 
-    p user.email
-    mail(:to => user.email,
-        :subject => "Password Reset")
+    mail(:to => user.email)
   end
 end
