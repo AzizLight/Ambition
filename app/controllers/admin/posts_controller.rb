e85907947aa23ce8949d106401c2d53bec5a5ad7
Aziz Light <aziz@azizlight.me>
2012-03-31 04:02:47 +0100

    Users can only edit their own post unless they're admins

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,14 @@
 class Admin::PostsController < Admin::BaseController
+  before_filter :restrict_access_to_authors, :only => [:edit, :update]
+  before_filter :restrict_access_to_admins, :only => [:destroy]
+
   def index
     @title = "Posts"
-    @posts = Post.order("created_at DESC").page(params[:page]).per(10)
+    if current_user.admin?
+      @posts = Post.order("created_at DESC").page(params[:page]).per(10)
+    else
+      @posts = Post.where("user_id = ?", current_user.id).order("created_at DESC").page(params[:page]).per(10)
+    end
   end
 
   def new
@@ -22,11 +29,9 @@ class Admin::PostsController < Admin::BaseController
 
   def edit
     @title = "Edit Post"
-    @post = Post.find_by_id(params[:id])
   end
 
   def update
-    @post = Post.find_by_id(params[:id])
     if @post.update_attributes(params[:post])
       flash[:success] = "Post updated successfully!"
       redirect_to admin_post_path(@post)
@@ -48,6 +53,24 @@ class Admin::PostsController < Admin::BaseController
       redirect_to admin_posts_path
     else
       redirect_to admin_post_path(@post), :alert => "Unable to delete the post..."
+    end
+  end
+
+  private
+
+  def restrict_access_to_authors
+    @post = Post.find_by_id(params[:id])
+    unless current_user.id == @post.user.id
+      flash[:warning] = "You are not allowed to access this page!"
+      redirect_back_or_to admin_posts_url
+    end
+  end
+
+  def restrict_access_to_admins
+    @post = Post.find_by_id(params[:id])
+    unless current_user.admin?
+      flash[:warning] = "You are not allowed to access this page!"
+      redirect_back_or_to admin_posts_url
     end
   end
 end
